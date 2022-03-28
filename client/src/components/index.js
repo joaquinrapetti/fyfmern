31ab1aa247fc524e2b642a5e6a3b43c807752f5a
joaquinrapetti <j.rapetti92@gmail.com>
2022-03-28 17:09:13 -0300

    form component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,6 @@
+import FormRow from './FormRow';
 import Logo from '../components/Logo';
 
-export { Logo }
+
+
+export { Logo, FormRow }
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { Logo, FormRow } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+import { useNavigate } from "react-router-dom";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
 
 const Register = () => {
+  const [values, setValues] = useState(initialState);
+  // glabal state and useNavigate
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
   return (
-    <div>Register</div>
-  )
-}
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        {/* name input */}
+        <FormRow
+          type="text"
+          name="name"
+          value={values.name}
+          handleChange={handleChange}
+        />
+        {/* email input */}
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* password input */}
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+      </form>
+    </Wrapper>
+  );
+};
 
 export default Register;
