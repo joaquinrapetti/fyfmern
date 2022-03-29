8bef622166ae0bace616619612c6d6c76257a144
joaquinrapetti <j.rapetti92@gmail.com>
2022-03-29 15:43:08 -0300

    cors and proxy

--- server.js
+++ server.js
@@ -1,45 +1,54 @@
+import cors from "cors";
 import express from "express";
 const app = express();
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import 'express-async-errors';
+import "express-async-errors";
 
 // db and authenticateUser
 import connectDB from "./db/connect.js";
 
 // routers
-import authRoutes from './routes/authRoutes.js'
-import travelRoutes from './routes/travelRoutes.js'
+import authRoutes from "./routes/authRoutes.js";
+import travelRoutes from "./routes/travelRoutes.js";
 
 // middleware
-notFoundMiddleware
+notFoundMiddleware;
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import notFoundMiddleware from "./middleware/not-found.js";
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+console.log("hello");
+console.log("hello");
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
+  // throw new Error('error')
+  res.json({ msg: "Welcome" });
+});
+
+app.get("/api/v1", (req, res) => {
     // throw new Error('error')
-    res.send('Welcome')
-})
+    res.json({ msg: "API" });
+  });
 
-app.use('/api/v1/auth', authRoutes)
-app.use('/api/v1/travels', travelRoutes)
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/travels", travelRoutes);
 
-app.use(notFoundMiddleware)
-app.use(errorHandlerMiddleware)
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URL)
-        app.listen(port, () => {
-            console.log(`Server is listening on port ${port}...`);
-        })
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-start()
+start();
